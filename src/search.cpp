8e766b0923eae29ffeac70601a7ee5a752e0bd8e
animakozak <animakozak@ex.ua>
2016-05-01 00:36:06 +0300

    added src folder and search.cpp [WIP]

---
+++ src/search.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct table{
+  string product;
+  int calories;
+};
+string calSearch(string query){
+  string respond;
+  table calTable[962];
+  return respond;
+}
